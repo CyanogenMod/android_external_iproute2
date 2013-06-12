7f28894dbd83c9d75c9cf2100469f5c2b0ab0515
JP Abgrall <jpa@google.com>
2013-06-12 11:50:40 -0700

    ip: update Android.mk for v3.4.0 new files.
    ipnetns and few others have appeared since the 2.6.35 update.
    
    Bug: 5095649
    Change-Id: Ifaf9135cb28e85e519c6983faf0ebc29f3e85dd8

--- ip/Android.mk
+++ ip/Android.mk
@@ -1,11 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := ip.c ipaddress.c ipaddrlabel.c iproute.c iprule.c   \
-	rtm_map.c iptunnel.c ip6tunnel.c tunnel.c ipneigh.c ipntable.c \
-	iplink.c ipmaddr.c ipmonitor.c ipmroute.c ipprefix.c ipxfrm.c  \
-	xfrm_state.c xfrm_policy.c xfrm_monitor.c iplink_vlan.c        \
-	link_veth.c link_gre.c iplink_can.c
+LOCAL_SRC_FILES := ip.c ipaddress.c ipaddrlabel.c iproute.c iprule.c ipnetns.c \
+        rtm_map.c iptunnel.c ip6tunnel.c tunnel.c ipneigh.c ipntable.c iplink.c \
+        ipmaddr.c ipmonitor.c ipmroute.c ipprefix.c iptuntap.c \
+        ipxfrm.c xfrm_state.c xfrm_policy.c xfrm_monitor.c \
+        iplink_vlan.c link_veth.c link_gre.c iplink_can.c \
+        iplink_macvlan.c iplink_macvtap.c ipl2tp.c
 
 LOCAL_MODULE := ip
 
